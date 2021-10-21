02cd9419c3eb270e29f226e7c8133a5d26118587
Maxim Mitin <maximus405@gmail.com>
2021-10-21 22:09:54 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Maxim Mitin on 21.10.21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
